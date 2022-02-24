838780e0d8ac198e51b860f7e2f8a45d99c84c86
sharexy23 <66343719+sharexy23@users.noreply.github.com>
2022-02-24 09:55:56 +0100

    Update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -49,7 +49,7 @@ export default class Login extends React.Component {
       email: this.state.email,
       password:this.state.password,
     }
-    axios.post('http://localhost:3000/login', userData)
+    axios.post('https://lottery-app-omotomiwa.herokuapp.com/login', userData)
       .then(res =>{
         if (res.data.message === "Login successful") {
 
